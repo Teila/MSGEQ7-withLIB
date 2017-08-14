8e52c893fd53b4a688ee6406d0c87bcac4da5703
Teila <inferno256@gmail.com>
2017-08-14 13:43:47 -0700

    Update MSGEQ7.ino
    UPDATE version
    UPDATE Color of Light

--- MSGEQ7.ino
+++ MSGEQ7.ino
@@ -6,7 +6,7 @@
 // FastLED
 #include "FastLED.h"
 
-#define Version "1.0.6ALT"
+#define Version "1.0.7ALT"
 
 #define LED_PINS    6 // DATA_PIN
 #define COLOR_ORDER GRB
@@ -130,7 +130,7 @@ unsigned long currentMillis = millis();
 void Lights() {
   for (int i = 0; i<NUM_LEDS; i++) {
   //leds[i] = CRGB( 60, 50, 50);
-  leds[i] = CRGB( 254, 240, 240);
+  leds[i] = CRGB( 254, 245, 245);
   }
   FastLED.setBrightness(BRIGHTNESS);
   FastLED.show();
